46644cf35bc152bb48a6a8777bd14e1282edd9bc
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-24 03:58:10 +0700

    Import fetchWorkGroupList and use setWorkGroupList

--- src/state/homework/effects/init.js
+++ src/state/homework/effects/init.js
@@ -1,5 +1,8 @@
 // @flow
-import type {Homework} from '../types'
+import type {
+  Homework,
+  WorkGroup,
+} from '../types'
 import type {Effect} from '../../types'
 import type {TransitionSuccessAction} from '../../../router/types'
 
@@ -12,13 +15,16 @@ import {actionTypes as routerTypes} from 'redux-router5'
 
 import {homeworkActions} from '../actions'
 import {fetchHomeworkList} from '../lib/fetchHomeworkList'
+import {fetchWorkGroupList} from '../lib/fetchWorkGroupList'
 
 export function* initEffect(): Effect {
   while (true) {
     const transition: TransitionSuccessAction = yield take(routerTypes.TRANSITION_SUCCESS)
     if (transition.payload.route.name === 'home') {
-      const homeworkList: Homework[] = yield call(fetchHomeworkList)
+      const homeworkList: ?Homework[] = yield call(fetchHomeworkList)
+      const workGroupList: ?WorkGroup[] = yield call(fetchWorkGroupList)
       yield put(homeworkActions.setHomeworkList(homeworkList))
+      yield put(homeworkActions.setWorkGroupList(workGroupList))
     }
   }
 }
