bf022bcaf35cc133190f99576d70156647f6bd73
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-24 03:44:54 +0700

    Remove unuse value

--- src/view/homework/HomeworkStatistic.js
+++ src/view/homework/HomeworkStatistic.js
@@ -2,18 +2,7 @@
 import type {Homework} from '../../state/homework/types'
 
 import React, { PureComponent } from 'react'
-import styled from 'styled-components'
 
-const CardContainer = styled.div`
-  padding-bottom: 1rem;
-`
-const BottomMenu = styled.div`
-  display: grid;
-  grid-template-columns: 30% 30% 30%;
-  grid-column-gap: 1rem;
-  justify-content: center;
-  padding-bottom: 1rem;
-`
 type Props = {
 }
 
