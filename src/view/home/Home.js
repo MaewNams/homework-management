7015fc1dc874cf0ee7debb1a859cc231c022fd25
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-24 03:22:48 +0700

    Add media-left, media in the middle

--- src/view/home/Home.js
+++ src/view/home/Home.js
@@ -4,10 +4,14 @@ import type {Homework} from '../../state/homework/types'
 
 import React, { PureComponent } from 'react'
 import {connect} from 'react-redux'
+import styled from 'styled-components'
 
 import {homeworkSelectors} from '../../state/homework/selectors'
 import {HomeworkList} from '../homework'
 
+const Container = styled.div`
+  padding: 1rem;
+`
 type Props = {
   homeworkList: ?Homework[]
 }
@@ -16,11 +20,66 @@ export class Home extends PureComponent<Props> {
   render() {
     const {homeworkList} = this.props
     return (
-     <div className="container is-fluid">
-       { homeworkList && (
-         <HomeworkList homeWorkList={homeworkList} />
-       )}
-     </div>
+     <Container className="container is-fluid">
+      <div className="media">
+        <div className="media-left">
+          <nav className="panel">
+            <a className="panel-block is-active">
+              <span className="panel-icon">
+                <i className="fas fa-book" aria-hidden="true"></i>
+              </span>
+              bulma
+            </a>
+            <a className="panel-block">
+              <span className="panel-icon">
+                <i className="fas fa-book" aria-hidden="true"></i>
+              </span>
+              marksheet
+            </a>
+            <a className="panel-block">
+              <span className="panel-icon">
+                <i className="fas fa-book" aria-hidden="true"></i>
+              </span>
+              minireset.css
+            </a>
+            <a className="panel-block">
+              <span className="panel-icon">
+                <i className="fas fa-book" aria-hidden="true"></i>
+              </span>
+              jgthms.github.io
+            </a>
+            <a className="panel-block">
+              <span className="panel-icon">
+                <i className="fas fa-code-branch" aria-hidden="true"></i>
+              </span>
+              daniellowtw/infboard
+            </a>
+            <a className="panel-block">
+              <span className="panel-icon">
+                <i className="fas fa-code-branch" aria-hidden="true"></i>
+              </span>
+              mojs
+            </a>
+            <label className="panel-block">
+              <input type="checkbox" />
+              remember me
+            </label>
+            <div className="panel-block">
+              <button className="button is-link is-outlined is-fullwidth">
+                reset all filters
+              </button>
+            </div>
+          </nav>
+        </div>
+        <div className="media-content">
+          <div className="content">
+          { homeworkList && (
+            <HomeworkList homeWorkList={homeworkList} />
+          )}
+          </div>
+        </div>
+      </div>
+     </Container>
     )
   }
 }
