1b00894bd2fd29850618157c746c8094e31a86f4
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-27 04:54:42 +0700

    Update createHomework

--- src/state/homework/effects/createHomework.js
+++ src/state/homework/effects/createHomework.js
@@ -1,11 +1,31 @@
 // @flow
-
+import type {User} from '../../session/types'
 import type {Effect} from '../../types'
 
 import {
-
+  call,
+  put,
+  select,
+  take,
 } from 'redux-saga/effects'
+import {actions} from 'redux-router5'
+
+import {homeworkActions} from '../actions'
+import {sessionSelectors} from '../../session/selectors'
+import {saveHomeworkToFireStore} from '../lib/saveHomeworkToFireStore'
+import {generateHomeworkId} from '../lib/generateHomeworkId'
 
 export function* createHomeworkEffect(): Effect {
-  // TODO: put your effect logic here
+  while (true) {
+    const {payload: {subject, detail, date}} = yield take(homeworkActions.createHomework.id)
+    const user: User = yield select(state => sessionSelectors.getUser(state))
+    const homeworkId: string = yield call(generateHomeworkId)
+    try {
+      yield call(saveHomeworkToFireStore, homeworkId, subject, detail, date, user)
+      yield put(actions.navigateTo('home'))
+    }
+    catch(error){
+      console.log('error', error)
+    }
+  }
 }
