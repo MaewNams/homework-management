a52447bf9b5b38b97f36dba179ea1f31079da7b3
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-27 04:51:20 +0700

    Update firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,10 +6,10 @@ import 'firebase/storage'
 let config;
     config = {
       apiKey: "AIzaSyBx-8sLarNTmxc2FuJO0lth4rzHIyRwndc",
-      authDomain: "homework-management-2e619",
-      databaseURL: "https://homework-management-2e619.firebaseio.com/",
-      projectId: "project-361577444410",
-      storageBucket: "gs://homework-management-2e619.appspot.com",
+      authDomain: "homework-management-2e619.firebaseapp.com",
+      databaseURL: "https://homework-management-2e619.firebaseio.com",
+      projectId: "homework-management-2e619",
+      storageBucket: "homework-management-2e619.appspot.com",
       messagingSenderId: "361577444410"
     }
  
