41b72fa34380a132c0d15e4259ae446734493cf9
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-27 02:47:48 +0700

    Add user in props. Add user info

--- src/view/home/Home.js
+++ src/view/home/Home.js
@@ -4,12 +4,17 @@ import type {
   Homework,
   WorkGroup,
 } from '../../state/homework/types'
+import type {
+  User
+} from '../../state/session/types'
 
 import React, { PureComponent } from 'react'
 import {connect} from 'react-redux'
 import {actions} from 'redux-router5'
 import styled from 'styled-components'
 
+import {sessionActions} from '../../state/session/actions'
+import {sessionSelectors} from '../../state/session/selectors'
 import {homeworkSelectors} from '../../state/homework/selectors'
 import {
   HomeworkList,
@@ -25,11 +30,19 @@ const CreateButton = styled.div`
   padding-bottom: 0.5rem;
   flex-direction: row-reverse;
 `
+const SignOutButton = styled.a`
+  margin-bottom: 2.5rem;
+`
+const UserInfo = styled.h5`
+  text-align: center;
+`
 
 type Props = {
   homeworkList: ?Homework[],
   workGroupList: ?WorkGroup[],
   navigateTo: Function,
+  signOut: Function,
+  user: ?User,
 }
 
 export class Home extends PureComponent<Props> {
@@ -38,12 +51,26 @@ export class Home extends PureComponent<Props> {
     navigateTo('create-homework')
   }
 
+  handleSignOut = (e:any):void => {
+    e.preventDefault()
+    const {signOut} = this.props
+    signOut()
+  }
+
   render() {
-    const {homeworkList, workGroupList, navigateTo} = this.props
+    const {homeworkList, workGroupList, navigateTo, user} = this.props
     return (
      <Container className="container is-fluid">
       <div className="media">
         <div className="media-left">
+          {user && (
+            <UserInfo className="subtitle is-5">
+              <i className="fa fa-user"></i> {user.email}
+            </UserInfo>
+          )}
+          <SignOutButton onClick={this.handleSignOut} className="button is-danger is-outlined is-fullwidth">
+            ออกจากระบบ
+          </SignOutButton>
           <GroupWorkPanel workGroupList={workGroupList} />
         </div>
         <div className="media-content">
@@ -72,11 +99,13 @@ const mapStateToProps = (state: ApplicationState, props: Props) => {
   return {
     homeworkList: homeworkSelectors.getHomeworkList(state),
     workGroupList: homeworkSelectors.getWorkGroupList(state),
+    user: sessionSelectors.getUser(state),
   }
 }
 
 const withStore = connect(mapStateToProps, {
-  navigateTo: actions.navigateTo
+  navigateTo: actions.navigateTo,
+  signOut: sessionActions.signOut,
 })
 
 export default withStore(Home)
