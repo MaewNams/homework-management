0c92d38fc7b774429312c7d207460f1a23c64bee
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-27 02:10:36 +0700

    Update signOut effect

--- src/state/session/effects/signOut.js
+++ src/state/session/effects/signOut.js
@@ -3,9 +3,21 @@
 import type {Effect} from '../../types'
 
 import {
-
+  call,
+  put,
+  take,
 } from 'redux-saga/effects'
+import {actions} from 'redux-router5'
+
+import {sessionActions} from '../actions'
+import {signOut} from '../lib/signOut'
 
 export function* signOutEffect(): Effect {
   // TODO: put your effect logic here
+  while (true) {
+    yield take(sessionActions.signOut.id)
+    yield call(signOut)
+    yield put(sessionActions.setUser(null))
+    yield put(actions.navigateTo('sign-in'))
+  }
 }
