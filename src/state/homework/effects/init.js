14f8e45b4a9224e9c6cb5f51477262746f2168fc
MaewNams <ruttikar_maya@hotmail.com>
2019-03-27 17:54:42 +0700

    Import sessionActions

--- src/state/homework/effects/init.js
+++ src/state/homework/effects/init.js
@@ -3,17 +3,22 @@ import type {
   Homework,
   WorkGroup,
 } from '../types'
+import type {
+  User,
+} from '../../session/types'
 import type {Effect} from '../../types'
 import type {TransitionSuccessAction} from '../../../router/types'
 
 import {
   call,
   put,
+  select,
   take,
 } from 'redux-saga/effects'
 import {actionTypes as routerTypes} from 'redux-router5'
 
 import {homeworkActions} from '../actions'
+import {sessionSelectors} from '../../session/selectors'
 import {fetchHomeworkList} from '../lib/fetchHomeworkList'
 import {fetchWorkGroupList} from '../lib/fetchWorkGroupList'
 
@@ -21,10 +26,13 @@ export function* initEffect(): Effect {
   while (true) {
     const transition: TransitionSuccessAction = yield take(routerTypes.TRANSITION_SUCCESS)
     if (transition.payload.route.name === 'home') {
-      const homeworkList: ?Homework[] = yield call(fetchHomeworkList)
-      const workGroupList: ?WorkGroup[] = yield call(fetchWorkGroupList)
-      yield put(homeworkActions.setHomeworkList(homeworkList))
-      yield put(homeworkActions.setWorkGroupList(workGroupList))
+      const user: User = yield select(state => sessionSelectors.getUser(state))
+      if (user) {
+        const homeworkList: ?Homework[] = yield call(fetchHomeworkList, user)
+        const workGroupList: ?WorkGroup[] = yield call(fetchWorkGroupList)
+        yield put(homeworkActions.setHomeworkList(homeworkList))
+        yield put(homeworkActions.setWorkGroupList(workGroupList))
+      }
     }
   }
 }
