ca42b50c6647b8157d02c54f8fcacd2d9ad73a34
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-28 04:24:43 +0700

    Update deleteHomework payload

--- src/state/homework/actions/deleteHomework.js
+++ src/state/homework/actions/deleteHomework.js
@@ -2,15 +2,15 @@
 import type {Action} from '../../../state/types'
 
 export type DeleteHomeworkPayload = {
-  // TODO: Add payload content
+  homeworkId: string
 }
 
 const id = 'homework/DELETE_HOMEWORK'
 
-export const deleteHomework = (): Action<DeleteHomeworkPayload> => ({
+export const deleteHomework = (homeworkId: string): Action<DeleteHomeworkPayload> => ({
   type: id,
   payload: {
-    // TODO: Add payload content
+    homeworkId
   }
 })
 
