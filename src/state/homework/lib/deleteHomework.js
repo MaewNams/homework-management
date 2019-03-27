5bf8c71b49b62f6027dd496d7e6f468afbcc68ca
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-28 04:23:56 +0700

    Create delteHomework in lib

---
+++ src/state/homework/lib/deleteHomework.js
@@ -0,0 +1,12 @@
+// @flow
+import type {User} from '../../session/types'
+
+import {
+  firebaseRef as firebase,
+  db,
+} from '../../../firebase'
+
+export async function deleteHomework(user: User, homeworkId: string): Promise<any> {
+    await db.collection('homeworks').doc(user.uid).where('id' === homeworkId).delete()
+}
+
