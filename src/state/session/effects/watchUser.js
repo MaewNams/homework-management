4e759b1bf51b88956e3b4d611793c407163df472
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-28 05:24:01 +0700

    Add fetchGroupList

--- src/state/session/effects/watchUser.js
+++ src/state/session/effects/watchUser.js
@@ -13,6 +13,7 @@ import {actions} from 'redux-router5'
 import {observerUser} from './observerUser'
 import {sessionActions} from '../actions'
 import {fetchHomeworkList} from '../../homework/lib/fetchHomeworkList'
+import {fetchWorkGroupList} from '../../homework/lib//fetchWorkGroupList'
 import {homeworkActions} from '../../homework/actions'
 
 export function* watchUserEffect(): Effect {
@@ -24,7 +25,9 @@ export function* watchUserEffect(): Effect {
         yield put(sessionActions.setUser(null))
       } else {
         yield put(sessionActions.setUser(user))
+        const workGroupList = yield call(fetchWorkGroupList)
         const homeworkList = yield call(fetchHomeworkList, user)
+        yield put(homeworkActions.setWorkGroupList(workGroupList))
         yield put(homeworkActions.setHomeworkList(homeworkList))
       }
     }
