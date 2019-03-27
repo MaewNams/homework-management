74c901e38dbd14a3c7f3165d166bb16b7e5c47e3
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-28 04:27:31 +0700

    Update deleteHomework effect

--- src/state/homework/effects/deleteHomework.js
+++ src/state/homework/effects/deleteHomework.js
@@ -1,11 +1,32 @@
 // @flow
-
+import type {User} from '../../session/types'
+import type {Homework} from '../types'
 import type {Effect} from '../../types'
 
 import {
-
+  call,
+  put,
+  select,
+  take,
 } from 'redux-saga/effects'
+import {actions} from 'redux-router5'
+
+import {homeworkActions} from '../actions'
+import {sessionSelectors} from '../../session/selectors'
+import {deleteHomework} from '../lib/deleteHomework'
+import {fetchHomeworkList} from '../lib/fetchHomeworkList'
 
 export function* deleteHomeworkEffect(): Effect {
-  // TODO: put your effect logic here
+  while (true) {
+    const {payload: {homeworkId}} = yield take(homeworkActions.deleteHomework.id)
+    const user: User = yield select(state => sessionSelectors.getUser(state))
+    try {
+      yield call(deleteHomework, user, homeworkId)
+      const homeworkList: ?Homework[] = yield call(fetchHomeworkList, user)
+      yield put(homeworkActions.setHomeworkList(homeworkList))
+    }
+    catch(error){
+      console.log('error', error)
+    }
+  }
 }
