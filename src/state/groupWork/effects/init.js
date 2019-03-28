1f12020b05627cf5a3b349618ed323a493e592d9
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-29 05:34:32 +0700

    Update init effect

--- src/state/groupWork/effects/init.js
+++ src/state/groupWork/effects/init.js
@@ -1,11 +1,34 @@
 // @flow
-
+import type {User} from '../../session/types'
+import type {Homework} from '../../homework/types'
 import type {Effect} from '../../types'
+import type {TransitionSuccessAction} from '../../../router/types'
 
 import {
-
+  call,
+  put,
+  select,
+  take,
 } from 'redux-saga/effects'
+import {actions} from 'redux-router5'
+import {actionTypes as routerTypes} from 'redux-router5'
+
+import {groupWorkActions} from '../actions'
+import {fetchGroupHomeworkList} from '../lib/fetchGroupHomeworkList'
+import {fetchGroupWorkList} from '../lib/fetchGroupWorkList'
+import {sessionSelectors} from '../../session/selectors'
 
 export function* initEffect(): Effect {
-  // TODO: put your effect logic here
+  while (true) {
+    const transition: TransitionSuccessAction = yield take(routerTypes.TRANSITION_SUCCESS)
+    if (transition.payload.route.name === 'group') {
+      const groupId  = transition.payload.route.params.id
+      const groupHomeworkList: Homework[] = yield call(fetchGroupHomeworkList, groupId)
+      const user: User = yield select(state => sessionSelectors.getUser(state))
+      const workGroupList = yield call(fetchGroupWorkList, user)
+      yield put(groupWorkActions.setGroupHomeworkList(groupHomeworkList))
+      yield put(groupWorkActions.setGroupWorkList(workGroupList))
+
+    }
+  }
 }
