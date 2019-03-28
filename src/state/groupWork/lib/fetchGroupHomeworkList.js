a429f15bb3b9a2509a67a80b157d42dd2e564488
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-29 03:41:30 +0700

    Create fetchGroupHomeworkList lib

---
+++ src/state/groupWork/lib/fetchGroupHomeworkList.js
@@ -0,0 +1,17 @@
+// @flow
+import type {Homework} from '../../homework/types'
+import type {User} from '../../session/types'
+
+import {db} from '../../../firebase'
+
+export async function fetchGroupHomeworkList(homeworkIds: string[]): Promise<?Homework[]> {
+  const groupHomeworkList = []
+
+  for (const id of homeworkIds) {
+    const groupSnapShot = await db.collection('groupHomeworks').doc(id).get()
+    const data = groupSnapShot.data()
+    groupHomeworkList.push(data)
+  }
+
+  return groupHomeworkList
+}
