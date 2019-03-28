3be2b6c88ce9c9f243c0f757118a350f28d7cba1
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-29 05:25:55 +0700

    Update fetchGroupHomeworkList

--- src/state/groupWork/lib/fetchGroupHomeworkList.js
+++ src/state/groupWork/lib/fetchGroupHomeworkList.js
@@ -3,14 +3,18 @@ import type {Homework} from '../../homework/types'
 
 import {db} from '../../../firebase'
 
-export async function fetchGroupHomeworkList(homeworkIds: string[]): Promise<?Homework[]> {
+export async function fetchGroupHomeworkList(groupId: string): Promise<?Homework[]> {
   const groupHomeworkList = []
+
+  const snapShot = await db.collection('groupWorks').doc(groupId).get()
+  const groupWorks = snapShot.data()
+  const homeworkIds = [...groupWorks.groupHomeworks]
 
   for (const id of homeworkIds) {
     const groupSnapShot = await db.collection('groupHomeworks').doc(id).get()
     const data = groupSnapShot.data()
     groupHomeworkList.push(data)
   }
-  console.log('groupHomeworkList', groupHomeworkList)
+
   return groupHomeworkList
 }
