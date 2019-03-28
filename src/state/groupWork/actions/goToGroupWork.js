bc0c9b53d4ec5b2631b46298a21f1831164ac80d
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-29 05:26:02 +0700

    Update payload

--- src/state/groupWork/actions/goToGroupWork.js
+++ src/state/groupWork/actions/goToGroupWork.js
@@ -3,16 +3,14 @@ import type {Action} from '../../../state/types'
 
 export type GoToGroupWorkPayload = {
   groupId: string,
-  homeworkIds: string[],
 }
 
 const id = 'groupWork/GO_TO_GROUP_WORK'
 
-export const goToGroupWork = (groupId: string, homeworkIds: string[]): Action<GoToGroupWorkPayload> => ({
+export const goToGroupWork = (groupId: string): Action<GoToGroupWorkPayload> => ({
   type: id,
   payload: {
     groupId,
-    homeworkIds
   }
 })
 
