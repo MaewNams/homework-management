4a55b077cedbf81edbd74fb90d20ea99f7af1eae
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-29 05:10:26 +0700

    Create saveGrouphomeworkToFireStore

---
+++ src/state/groupWork/lib/saveGroupHomeworkToFireStore.js
@@ -0,0 +1,25 @@
+// @flow
+import type {User} from '../../session/types'
+
+import {
+  firebaseRef as firebase,
+  db,
+} from '../../../firebase'
+
+export async function saveGroupHomeworkToFireStore(groupId: string, id: string, subject: string, detail: string, date: string, user: User): Promise<any> {
+  const homework = {
+    id: id,
+    owner: user.uid,
+    subject: subject,
+    detail: detail,
+    date: date,
+    status: 'ยังไม่เสร็จ'
+  }
+  db.collection('groupHomeworks').doc(id).set(homework)
+  const snapshort = await db.collection('groupWorks').doc(groupId).get()
+  const oldHomeworks = snapshort.data()
+  const homeworks = [...oldHomeworks.groupHomeworks, id]
+  db.collection('groupWorks').doc(groupId).set({groupHomeworks: homeworks}, {merge: true})
+
+}
+
