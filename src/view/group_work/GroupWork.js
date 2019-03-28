7c4895ec9b402759356dded8e7614b0cc2a5d2e9
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-03-29 03:52:05 +0700

    Update GroupWork

--- src/view/group_work/GroupWork.js
+++ src/view/group_work/GroupWork.js
@@ -2,44 +2,95 @@
 import type {ApplicationState} from '../../state/types'
 import type {
   Homework,
-  WorkGroup,
 } from '../../state/homework/types'
+import type {
+  User
+} from '../../state/session/types'
+import type {
+  GroupWork as GroupWorkType
+} from '../../state/groupWork/types'
 
 import React, { PureComponent } from 'react'
 import {connect} from 'react-redux'
+import {actions} from 'redux-router5'
 import styled from 'styled-components'
 
+import {sessionActions} from '../../state/session/actions'
+import {sessionSelectors} from '../../state/session/selectors'
+import {groupWorkSelectors} from '../../state/groupWork/selectors'
 import {homeworkSelectors} from '../../state/homework/selectors'
-import {
-  HomeworkList,
-  HomeworkStatistic,
-} from '../homework'
+import {homeworkActions} from '../../state/homework/actions'
+import {GroupHomeworkList} from './GroupHomeworkList'
 import {GroupWorkPanel} from '../common_component'
 
 const Container = styled.div`
   padding: 1rem;
 `
+const CreateButton = styled.div`
+  display: flex;
+  padding-bottom: 0.5rem;
+  flex-direction: row-reverse;
+`
+const SignOutButton = styled.a`
+  margin-bottom: 2.5rem;
+`
+const UserInfo = styled.h5`
+  text-align: center;
+`
+
 type Props = {
-  homeworkList: ?Homework[],
-  workGroupList: ?WorkGroup[],
+  deleteHomework: Function,
+  groupHomeworkList: ?Homework[],
+  groupWorkList: ?GroupWorkType[],
+  navigateTo: Function,
+  signOut: Function,
+  user: ?User,
 }
 
 export class GroupWork extends PureComponent<Props> {
+
+  handleGoToCreateHomework = (navigateTo: Function):void => {
+    navigateTo('create-homework')
+  }
+
+  handleSignOut = (e:any):void => {
+    e.preventDefault()
+    const {signOut} = this.props
+    signOut()
+  }
+
   render() {
-    const {homeworkList, workGroupList} = this.props
+    const {groupHomeworkList, groupWorkList, navigateTo, user, deleteHomework} = this.props
     return (
-     <Container className="container is-fluid">
-      <div className="media">
-        <div className="media-left">
-          <GroupWorkPanel workGroupList={workGroupList} />
-        </div>
-        <div className="media-content">
-          <HomeworkStatistic />
-          <div className="content">
+      <Container className="container is-fluid">
+        <div className="media">
+          <div className="media-left">
+            {user && (
+              <UserInfo className="subtitle is-5">
+                <i className="fa fa-user"></i> {user.email}
+              </UserInfo>
+            )}
+            <SignOutButton onClick={this.handleSignOut} className="button is-danger is-outlined is-fullwidth">
+              ออกจากระบบ
+            </SignOutButton>
+            <GroupWorkPanel groupWorkList={groupWorkList} />
+          </div>
+          <div className="media-content">
+            <CreateButton>
+              <a className="button is-primary" 
+                onClick={() => this.handleGoToCreateHomework(navigateTo)}
+              >
+                สร้างการบ้านใหม่
+              </a>
+            </CreateButton>
+            <div className="content">
+            { groupHomeworkList && (
+              <GroupHomeworkList deleteHomework={deleteHomework} homeWorkList={groupHomeworkList} />
+            )}
+            </div>
           </div>
         </div>
-      </div>
-     </Container>
+      </Container>
     )
   }
 }
@@ -47,13 +98,16 @@ export class GroupWork extends PureComponent<Props> {
 
 const mapStateToProps = (state: ApplicationState, props: Props) => {
   return {
-    homeworkList: homeworkSelectors.getHomeworkList(state),
-    workGroupList: homeworkSelectors.getWorkGroupList(state),
+    groupHomeworkList: homeworkSelectors.getHomeworkList(state),
+    groupWorkList: groupWorkSelectors.getGroupWorkList(state),
+    user: sessionSelectors.getUser(state),
   }
 }
 
 const withStore = connect(mapStateToProps, {
-
+  navigateTo: actions.navigateTo,
+  signOut: sessionActions.signOut,
+  deleteHomework: homeworkActions.deleteHomework
 })
 
 export default withStore(GroupWork)
