7a3ef31aa1a8aef7b5fd06a0be67864c33495251
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-04-08 01:02:30 +0700

    Import fetchGroupWork

--- src/state/groupWork/effects/goToGroupWork.js
+++ src/state/groupWork/effects/goToGroupWork.js
@@ -1,4 +1,5 @@
 // @flow
+import type {GroupWork} from '../types'
 import type {Homework} from '../../homework/types'
 import type {Effect} from '../../types'
 
@@ -11,12 +12,15 @@ import {actions} from 'redux-router5'
 
 import {groupWorkActions} from '../actions'
 import {fetchGroupHomeworkList} from '../lib/fetchGroupHomeworkList'
+import {fetchGroupWork} from '../lib/fetchGroupWork'
 
 export function* goToGroupWorkEffect(): Effect {
   while (true) {
     const {payload: {groupId}} = yield take(groupWorkActions.goToGroupWork.id)
     const groupHomeworkList: Homework[] = yield call(fetchGroupHomeworkList, groupId)
+    const groupWork: GroupWork = yield call(fetchGroupWork, groupId)
     try {
+      yield put (groupWorkActions.setGroupWork(groupWork))
       yield put(groupWorkActions.setGroupHomeworkList(groupHomeworkList))
       yield put(actions.navigateTo('group', {
         id: groupId
