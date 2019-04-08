5789fd4f9c193acb2b1f3ec8dcec955e41e157ae
Chonticha Chang <ruttikar_maya@hotmail.com>
2019-04-08 13:29:03 +0700

    Update deleteGroupHomeworkEffect

--- src/state/groupWork/effects/deleteGroupHomework.js
+++ src/state/groupWork/effects/deleteGroupHomework.js
@@ -1,11 +1,34 @@
 // @flow
-
+import type {User} from '../../session/types'
+import type {Homework} from '../../Homework/types'
 import type {Effect} from '../../types'
 
 import {
-
+  call,
+  put,
+  select,
+  take,
 } from 'redux-saga/effects'
+import {actions} from 'redux-router5'
+
+import {groupWorkActions} from '../actions'
+import {groupWorkSelectors} from '../selectors'
+import {deleteGroupHomework} from '../lib/deleteGroupHomework'
+import {deleteGroupHomeworkFromFirestore} from '../lib/deleteGroupHomeworkFromFirestore'
 
 export function* deleteGroupHomeworkEffect(): Effect {
-  // TODO: put your effect logic here
+  while (true) {
+    const {payload: {homeworkId}} = yield take(groupWorkActions.deleteGroupHomework.id)
+    try {
+      const groupWork = yield select(state => groupWorkSelectors.getGroupWork(state))
+      yield call(deleteGroupHomework, homeworkId)
+      yield call(deleteGroupHomeworkFromFirestore, groupWork.id, homeworkId)
+      const oldHomeworks = yield select(state => groupWorkSelectors.getGroupHomeworkList(state))
+      const newHomeworks = oldHomeworks.filter(homework => homework.id !== homeworkId)
+      yield put(groupWorkActions.setGroupHomeworkList(newHomeworks))
+    }
+    catch(error){
+      console.log('error', error)
+    }
+  }
 }
